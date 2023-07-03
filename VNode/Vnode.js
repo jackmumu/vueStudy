ad186f419a0a4fe8520a322dd49f5fab678bca25
jackmumu <1459307172@qq.com>
2023-07-03 16:32:49 +0800

    pref:keepalive、VNode

---
+++ VNode/Vnode.js
@@ -0,0 +1,41 @@
+  /**
+   * @internal
+   */
+  var VNode = /** @class */ (function () {
+    function VNode(tag, data, children, text, elm, context, componentOptions, asyncFactory) {
+        // 标签
+        this.tag = tag;
+        this.data = data;
+        this.children = children;
+        this.text = text;
+        this.elm = elm;
+        this.ns = undefined;
+        this.context = context;
+        this.fnContext = undefined;
+        this.fnOptions = undefined;
+        this.fnScopeId = undefined;
+        this.key = data && data.key;
+        this.componentOptions = componentOptions;
+        this.componentInstance = undefined;
+        this.parent = undefined;
+        this.raw = false;
+        this.isStatic = false;
+        this.isRootInsert = true;
+        this.isComment = false;
+        this.isCloned = false;
+        this.isOnce = false;
+        this.asyncFactory = asyncFactory;
+        this.asyncMeta = undefined;
+        this.isAsyncPlaceholder = false;
+    }
+    Object.defineProperty(VNode.prototype, "child", {
+        // DEPRECATED: alias for componentInstance for backwards compat.
+        /* istanbul ignore next */
+        get: function () {
+            return this.componentInstance;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return VNode;
+}());
---
+++ keepAlive/keepAlive.js
@@ -0,0 +1,88 @@
+var KeepAlive = {
+    name: 'keep-alive',
+    abstract: true,
+    props: {
+        include: patternTypes,
+        exclude: patternTypes,
+        max: [String, Number]
+    },
+    methods: {
+        cacheVNode: function () {
+            var _a = this, cache = _a.cache, keys = _a.keys, vnodeToCache = _a.vnodeToCache, keyToCache = _a.keyToCache;
+            if (vnodeToCache) {
+                var tag = vnodeToCache.tag, componentInstance = vnodeToCache.componentInstance, componentOptions = vnodeToCache.componentOptions;
+                cache[keyToCache] = {
+                    name: _getComponentName(componentOptions),
+                    tag: tag,
+                    componentInstance: componentInstance
+                };
+                keys.push(keyToCache);
+                // prune oldest entry
+                if (this.max && keys.length > parseInt(this.max)) {
+                    pruneCacheEntry(cache, keys[0], keys, this._vnode);
+                }
+                this.vnodeToCache = null;
+            }
+        }
+    },
+    created: function () {
+        this.cache = Object.create(null);
+        this.keys = [];
+    },
+    destroyed: function () {
+        for (var key in this.cache) {
+            pruneCacheEntry(this.cache, key, this.keys);
+        }
+    },
+    mounted: function () {
+        var _this = this;
+        this.cacheVNode();
+        this.$watch('include', function (val) {
+            pruneCache(_this, function (name) { return matches(val, name); });
+        });
+        this.$watch('exclude', function (val) {
+            pruneCache(_this, function (name) { return !matches(val, name); });
+        });
+    },
+    updated: function () {
+        this.cacheVNode();
+    },
+    render: function () {
+        var slot = this.$slots.default;
+        var vnode = getFirstComponentChild(slot);
+        var componentOptions = vnode && vnode.componentOptions;
+        if (componentOptions) {
+            // check pattern
+            var name_2 = _getComponentName(componentOptions);
+            var _a = this, include = _a.include, exclude = _a.exclude;
+            if (
+            // not included
+            (include && (!name_2 || !matches(include, name_2))) ||
+                // excluded
+                (exclude && name_2 && matches(exclude, name_2))) {
+                return vnode;
+            }
+            var _b = this, cache = _b.cache, keys = _b.keys;
+            var key = vnode.key == null
+                ? // same constructor may get registered as different local components
+                    // so cid alone is not enough (#3269)
+                    componentOptions.Ctor.cid +
+                        (componentOptions.tag ? "::".concat(componentOptions.tag) : '')
+                : vnode.key;
+            if (cache[key]) {
+                vnode.componentInstance = cache[key].componentInstance;
+                // make current key freshest
+                remove$2(keys, key);
+                keys.push(key);
+            }
+            else {
+                // delay setting the cache until update
+                this.vnodeToCache = vnode;
+                this.keyToCache = key;
+            }
+            // @ts-expect-error can vnode.data can be undefined
+            vnode.data.keepAlive = true;
+        }
+        return vnode || (slot && slot[0]);
+    }
+};
